f98ed69b0d649a469372df405011330c6eafa29c
Mateus Tormes <mateustormes@gmail.com>
2026-02-02 20:20:39 -0300

    removendo server repetido

--- server copy/game/map/Tile.js
+++
@@ -1,9 +0,0 @@
-export class Tile {
-    constructor(data) {
-        this.type = data.type || 'grass';
-        this.walkable = data.walkable !== false;
-        this.x = data.x || 0;
-        this.y = data.y || 0;
-        this.z = data.z || 1;
-    }
-}
