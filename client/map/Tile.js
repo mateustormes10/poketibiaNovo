d24b377b2907dd894c9bd8fe829625aeebaf3135
Mateus Tormes <mateustormes@gmail.com>
2026-02-12 13:02:58 -0300

    remocacao de scripts antigos

--- client/map/Tile.js
+++
@@ -1,8 +0,0 @@
-export class Tile {
-    constructor(data) {
-        this.type = data.type || 'grass';
-        this.walkable = data.walkable !== false;
-        this.sprite = data.sprite || null;
-        this.layer = data.layer || 0;
-    }
-}
--- client/network/ScanProtocol.js
+++
@@ -1,9 +0,0 @@
-// Protocolo de scanner para o client
-export const ClientEvents = {
-    ...window.ClientEvents,
-    SCAN: 'scan'
-};
-export const ServerEvents = {
-    ...window.ServerEvents,
-    SCAN_RESULT: 'scanResult'
-};
